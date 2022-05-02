acbe4a1d5f063749497a668cf61540ea4cb2e38f
Subbu Alagappan <s@subbu.dev>
2022-05-02 22:45:08 +0200

    Moved imports to right places

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,35 @@
 use std::fmt::Result;
 
+use crossterm::{
+  event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+  execute,
+  terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tui::{
+  backend::{Backend, CrosstermBackend},
+  layout::{Constraint, Direction, Layout},
+  style::{Color, Modifier, Style},
+  text::{Span, Spans, Text},
+  widgets::{Block, Borders, List, ListItem, Paragraph},
+  Frame, Terminal,
+};
+use unicode_width::UnicodeWidthStr;
+
 enum InputMode {
   Normal,
   Editing,
 }
+
+// fn print_todos(window: &Window, todos: &[Todo]) {
+//     todos.iter().for_each(|todo | {
+//         window.
+//         window.printw(format!(
+//             "[{}] {}\n",
+//             if todo.is_complete.unwrap_or(false) { '🔥' } else { ' ' },
+//             todo.content.as_ref().unwrap(),
+//         ));
+//     });
+// }
 
 pub struct App {
   /// Current value of the input box
